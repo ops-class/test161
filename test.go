77d1db763dda685548b4a42ce74b8deceac791d3
Geoffrey Challen <challen@buffalo.edu>
2015-12-28 21:50:28 -0600

    Fixing previous commit.

--- test.go
+++ test.go
@@ -1,17 +1,30 @@
 package test161
 
 import (
+	"errors"
 	"github.com/ericaro/frontmatter"
+	"github.com/termie/go-shutil"
 	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
 	// "github.com/jamesharr/expect"
 	// "gopkg.in/yaml.v2"
 )
 
+type Conf struct {
+	CPUs   uint8  `yaml:"cpus"`
+	RAM    string `yaml:"ram"`
+	Memory uint32
+}
+
 type Test struct {
 	Name        string   `yaml:"name"`
 	Description string   `yaml:"description"`
-	Tags        []string `yaml:"tags,flow"`
-	Depends     []string `yaml:"depends,flow"`
+	Tags        []string `yaml:"tags"`
+	Depends     []string `yaml:"depends"`
+	Config      Conf     `yaml:"conf"`
 	Content     string   `fm:"content" yaml:"-"`
 }
 
@@ -22,5 +35,51 @@ func LoadTest(filename string) (*Test, error) {
 	}
 	test := new(Test)
 	err = frontmatter.Unmarshal(data, test)
+	if test.Config.RAM != "" {
+		if unicode.IsDigit(rune(test.Config.RAM[len(test.Config.RAM)-1])) {
+			RAM, err := strconv.Atoi(test.Config.RAM)
+			if err != nil {
+				return nil, err
+			}
+			test.Config.Memory = uint32(RAM)
+		} else {
+			number, err := strconv.Atoi(test.Config.RAM[0 : len(test.Config.RAM)-1])
+			if err != nil {
+				return nil, err
+			}
+			multiplier := strings.ToUpper(string(test.Config.RAM[len(test.Config.RAM)-1]))
+			if multiplier == "K" {
+				test.Config.Memory = uint32(1024 * number)
+			} else if multiplier == "M" {
+				test.Config.Memory = uint32(1024 * 1024 * number)
+			} else {
+				return nil, errors.New("test161: could not convert RAM to integer")
+			}
+		}
+	}
 	return test, err
+}
+
+func (*Test) Run(kernel string, root string, tempRoot string) error {
+	tempDir, err := ioutil.TempDir(tempRoot, "test161")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tempDir)
+
+	if root != "" {
+		err = shutil.CopyTree(root, tempDir, nil)
+		if err != nil {
+			return err
+		}
+	}
+
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	defer os.Chdir(currentDir)
+	os.Chdir(tempDir)
+
+	return nil
 }
--- test_test.go
+++ test_test.go
@@ -9,17 +9,28 @@ import (
 func TestLoad(t *testing.T) {
 	assert := assert.New(t)
 
-	test, err := LoadTest("./fixtures/boot.yml")
+	test, err := LoadTest("./fixtures/tests/boot.yml")
 	assert.Nil(err)
 	assert.Equal(test.Name, "boot")
 	assert.NotNil(test.Description)
 	assert.Equal(reflect.DeepEqual(test.Tags, []string{"basic", "setup"}), true)
 	assert.Nil(test.Depends)
+	assert.Equal(test.Config.CPUs, (uint8)(4))
+	assert.Equal(test.Config.Memory, (uint32)(16777216))
 
-	test, err = LoadTest("./fixtures/shell.yml")
+	test, err = LoadTest("./fixtures/tests/shell.yml")
 	assert.Nil(err)
 	assert.Equal(test.Name, "shell")
 	assert.Equal(test.Description, "")
 	assert.Nil(test.Tags)
 	assert.Equal(reflect.DeepEqual(test.Depends, []string{"boot"}), true)
+	assert.Equal(test.Config.Memory, (uint32)(16777216))
+}
+
+func TestRun(t *testing.T) {
+	assert := assert.New(t)
+
+	test, err := LoadTest("./fixtures/tests/boot.yml")
+	assert.Nil(err)
+	err = test.Run("./fixtures/ASST0/kernel", "", "")
 }
