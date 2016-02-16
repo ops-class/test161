6fdcd9baa464e82842e63fa3b7ef6661245d2dbb
Scott Haseley <shaseley@buffalo.edu>
2016-02-16 00:24:24 -0500

    Forgot to add collab file in prev commit

---
+++ collab.go
@@ -0,0 +1,26 @@
+package test161
+
+var collabMsgs = map[string]string{
+	"asst1": CollabMsgAsst1,
+}
+
+const CollabMsgAsst1 = `
+ - Pair programming to complete the ASST1 implementation tasks is strongly encouraged.
+
+ - Answering the ASST1 code reading questions side-by-side with your partner is 
+   strongly encouraged.
+ 
+ - Helping other students with Git, GDB, editing, and with other parts of the OS/161
+   toolchain is encouraged.
+
+ - Discussing the code reading questions and browsing the source tree with other students 
+   is encouraged.
+
+ - Dividing the code reading questions and development tasks between partners is discouraged.
+
+ - Copying any answers from anyone who is not your partner or anywhere else and submitting
+   them as your own is cheating.
+
+ - You may not refer to or incorporate any external sources without explicit permission.
+
+`
